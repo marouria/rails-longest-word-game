a29208aab84dd51ce1d8fb0f8e2165b38dd6a9e4
Marie Leuliette <marie.leuliette@gmail.com>
2021-06-07 16:53:09 +0200

    letters and numbers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'games#home'
+  get 'new', to: 'games#new'
+  get 'score', to: 'games#score'
+  post 'score(.:format)', to: 'games#score'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# games/score(.:format)
